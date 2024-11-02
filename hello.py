05379e96989118d3a192366c1653abb21b80b8f0
Thalys Souza <thalyssouza95@gmail.com>
2024-11-01 23:17:16 -0300

    Adicionada a pasta .vscode ao gitignore Novo arquivo: cadastro_de_produto.py Novo arquivo: escola_v1_com_listas.py Novo arquivo: escola_v2_com_sets.py Novo arquivo: infixcalc.py (vazio por enquanto) Novo arquivo: tabuada.py Modificado: hello.py (otimização de codigo)

--- hello.py
+++ hello.py
@@ -10,29 +10,51 @@ Tenha a variável LANG devidamente configurada ex:
 
     export LANG=pt_BR
 
+Ou informe através do CLI argument '--lang'
+
+Ou o usuário terá que digitar manualmente o idioma.
+
 Execução:
 
     python3 hello.py
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.3"
 __author__ = "Thalys Souza"
 __license__ = "Unlicensed"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {"lang": None, "count": 1,} 
 
-msg = "Hello, world!"
+for arg in sys.argv[1:]: 
+    # TODO tratar valueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid option: '{key}'")
+        sys.exit()
+    arguments[key] = value
 
-if current_language =="pt_BR":
-	msg = "Olá, mundo!"
-elif current_language == "it_IT":
-	msg = "Ciao, mondo!"
-elif current_language == "es_SP":
-	msg = "Hola, mundo!"
-elif current_language == "fr_FR":
-	msg = "Bonjour, monde!!"
+current_language = arguments["lang"]
+if current_language is None:
+    #TODO usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language: ")
+
+current_language = current_language[:5] # fatia para pegar os perimeiros 5 caracteres
+
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_ES": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
 	 
-print(msg)
+print(msg[current_language] * int(arguments["count"]))
